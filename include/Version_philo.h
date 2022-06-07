d022ba93c7ce5074039937d0ad1aa1e7e11be37f
RoKerjea <rokerjea@student.42.fr>
2022-06-07 19:48:10 +0200

    V.1 by rokerjea at 07 06 2022 19h48 CEST

--- include/Version_philo.h
+++ include/Version_philo.h
@@ -1,7 +1,4 @@
 
-#ifndef VERSION_PHILO_H
-# define VERSION_PHILO_H
-# define MAJOR_VERSION "0"
-# define MINOR_VERSION "23"
-# define BUILD_DATE "07 06 2022 19h47 CEST"
-#endif
+#define MAJOR_VERSION "0"
+#define MINOR_VERSION "23"
+#define BUILD_DATE "07 06 2022 19h47 CEST"
